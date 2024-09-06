f881cf3147e234b41d60e2852a09c869859a2ddc
YASSINE ZEBIRI <yassinezebiri07@gmail.com>
2024-09-06 22:58:04 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/radio/sw.js', { scope: '/radio/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
